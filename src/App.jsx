c445b21f82a3c3a4f6469a0b99a7d1222ac0fa30
kaziarman23 <kaziarman@proton.me>
2024-05-02 22:42:13 +0600

    button buged not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(10)
+    const [count, setCount] = useState(10);
+    const [addButtonDisabled, setAddButton] = useState(false);
+    const [removeButtonDisabled, setRemoveButton] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Counter App</h1>
-      <h2>Point : {count}</h2>
-      <button>Add points</button>
-      <br />
-      <br />
-      <button>Remove points</button>
+    const AddValue = function () {
+        if (count !== 20) {
+            setAddButton(false);
+            setCount(count + 1);
+        } else {
+            setAddButton(true);
+        }
+    };
 
+    const RemoveValue = function () {
+        if (count !== 0) {
+            setRemoveButton(false);
+            setCount(count - 1);
+        } else {
+            setRemoveButton(true);
+        }
+    };
 
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>React Counter App</h1>
+            <h2>Point : {count}</h2>
+            <button disabled={addButtonDisabled} onClick={AddValue}>
+                Add points
+            </button>
+            <br />
+            <br />
+            <button disabled={removeButtonDisabled} onClick={RemoveValue}>
+                Remove points
+            </button>
+        </>
+    );
 }
 
-export default App
+export default App;
