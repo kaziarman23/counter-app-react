6cf7be513be7c0664a183d302cc3d881aeb25123
kaziarman23 <kaziarman@proton.me>
2024-05-02 11:38:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(10)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React Counter App</h1>
+      <h2>Point : {count}</h2>
+      <button>Add points</button>
+      <br />
+      <br />
+      <button>Remove points</button>
+
+
+    </>
+  )
+}
+
+export default App
